77f164f7328fe9cb8896ff7fda863380862bb440
Kyle Hubbard <kyle.james.hubbard@gmail.com>
2020-07-29 11:55:13 -0700

    create controller for Restaurants
    Co-Authored-By: Taylor Phillips <taylorphillips133@gmail.com>
    Co-authored-by: Tristan Emmerson <tristan@stickerslug.com>

---
+++ BestRestaurants/Controllers/RestaurantsController.cs
@@ -0,0 +1,57 @@
+using Microsoft.AspNetCore.Mvc;
+using BestRestaurants.Models;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace BestRestaurants.Controllers
+{
+  public class RestaurantsController : Controllers{
+  {
+    private readonly BestRestaurantsContext _db;
+
+    public RestaurantsController(BestRestaurantsContext db)
+    {
+      _db = db;
+    }
+
+    public ActionResult Index()
+    {
+      List<Restaurant> model = _db.Restaurants.Include(restaurants => restaurants.Cuisine).ToList();
+      return View(model);
+    }
+
+    public ActionResult Create()
+    {
+      ViewBag.CuisineId = new SelectList(_deb.Cuisines, "CategoryId", "Name");
+      return View();
+    }
+
+    [HttpPost]
+    public ActionResult Create(Restaurant restaurant)
+    {
+      _db.Restaurants.Add(restaurant);
+      _db.SaveChanges();
+      return RedirectToAction("Index");
+    }
+
+    public ActionResult Details(int id)
+    {
+      Restaurant thisRestaurant = _db.Restaurants.FirstOrDefault(restaurants => restaurants.RestaurantId == id);
+      return View(thisRestaurant);
+    }
+
+    public ActionResult Edit(int id)
+    {
+      var thisRestaurant = _db.Restaurants.FirstOrDefault(restaurants => restaurants.RestaurantId == id);
+      ViewBag.CuisineId = new SelectList(_db.Cuisines, "CuisineId", "Name");
+      return View(thisRestaurant);
+    }
+
+    // HTtp post Edit to start after lunch
+
+
+  }
+  }
+}
